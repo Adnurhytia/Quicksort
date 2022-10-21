1b9fd30ed3f16d8a21cda6c62827ea106fd818b2
Adnurhytia <adindanurhayati30@gmail.com>
2022-10-21 16:59:41 +0700

    Membuat class program dan menginput angka ke private int

--- Quicksort/Program.cs
+++ Quicksort/Program.cs
@@ -6,7 +6,7 @@ namespace QuickSort
     {
         //Array of integers to hold values
         private int[] arr = new int[20];
-        private int[] cmp_count = 0; //number of comparasion
+        private int cmp_count = 0; //number of comparasion
         private int mov_count = 0; //number of data movements
 
         //number of elements in array
