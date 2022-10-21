603737f89a30f9821d456a85444dec51521459ff
Adnurhytia <adindanurhayati30@gmail.com>
2022-10-21 17:32:34 +0700

    Menggunakan while untuk mencari element

--- Quicksort/Program.cs
+++ Quicksort/Program.cs
@@ -47,5 +47,39 @@ namespace QuickSort
             arr[y] = temp;
         }
 
+        public void q_sort(int low, int high)
+        {
+            int pivot, i, j;
+            if (low > high)
+                return;
+
+            //Partition the list into two parts;
+            //one containing elements less that or equal to pivot
+            //Outher containing elements greather than pivot
+
+            i = low + 1;
+            j = high;
+
+            pivot = arr[low];
+            while (i <= j)
+            {
+                //search for an element greater than pivot
+                while ((arr[i] <= pivot) && ( i <= high))
+                {
+                    i++;
+                    cmp_count++;
+                }
+                cmp_count++;
+
+                //search for an element less than or equal to pivot
+                while ((arr[j] > pivot) && ( j >= low))
+                {
+                    j--;
+                    cmp_count++;
+                }
+                cmp_count++;
+            }
+        }
+
     }
 }
